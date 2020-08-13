282f09f71b9e23c1918cbb599649a063d453fb6a
Sergiu Chilat <chilatsergiu@gmail.com>
2020-08-13 20:02:04 +0300

    Add Car scenario

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,28 @@
 public class Car {
+    private String name;
+    private Integer speed;
 
+    public void startEngine(Integer initialSpeed){
+        this.speed = initialSpeed;
+        System.out.println("Engine started");
+    }
+
+    public void speedUp(Integer delta) {
+        this.speed += delta;
+        System.out.println("Speed up by " + delta.toString());
+    }
+
+    public void speedDown(Integer delta) {
+        this.speed -= delta;
+        System.out.println("Speed down by " + delta.toString());
+    }
+
+    public void stopEngine() {
+        System.out.println("Engine stopped");
+        this.speed = 0;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
         Car car = new Car();
+        car.startEngine(3);
+        System.out.println("Current speed is " + car.getSpeed());
+        car.speedUp(2);
+        System.out.println("Current speed is " + car.getSpeed());
+        car.speedDown(3);
+        System.out.println("Current speed is " + car.getSpeed());
+        car.stopEngine();
+        System.out.println("Current speed is " + car.getSpeed());
     }
 }
