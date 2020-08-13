6535062ece69267538a71557f9f41c89f32c036e
Sergiu Chilat <chilatsergiu@gmail.com>
2020-08-13 20:12:48 +0300

    Add 2 cars scenario

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,6 @@
 public class Car {
-    public Car(Integer maxSpeed){
+    public Car(String name, Integer maxSpeed){
+        this.name = name;
         this.maxSpeed = maxSpeed;
     }
     private String name;
@@ -8,29 +9,34 @@ public class Car {
 
     public void startEngine(Integer initialSpeed){
         this.speed = initialSpeed;
-        System.out.println("Engine started");
+        System.out.println("[" + this.name + "] Engine started");
     }
 
     public void speedUp(Integer delta) {
         if(this.speed + delta > this.maxSpeed) {
-            System.out.println("Warning. Max speed exceeded");
+            System.out.println("[" + this.name + "] Warning. Max speed exceeded");
             return;
         }
         this.speed += delta;
-        System.out.println("Speed up by " + delta.toString());
+        System.out.println("[" + this.name + "] Speed up by " + delta.toString());
     }
 
     public void speedDown(Integer delta) {
         this.speed = Math.max(this.speed - delta, 0);
-        System.out.println("Speed down by " + delta.toString());
+        System.out.println("[" + this.name + "] Speed down by " + delta.toString());
     }
 
     public void stopEngine() {
-        System.out.println("Engine stopped");
+        System.out.println("[" + this.name + "] Engine stopped");
         this.speed = 0;
     }
 
     public int getSpeed() {
         return this.speed;
     }
+
+    public String getName() {
+        return this.name;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car(240);
-        car.startEngine(3);
-        System.out.println("Current speed is " + car.getSpeed());
-        car.speedUp(2);
-        System.out.println("Current speed is " + car.getSpeed());
-        car.speedDown(3);
-        System.out.println("Current speed is " + car.getSpeed());
-        car.stopEngine();
-        System.out.println("Current speed is " + car.getSpeed());
+        Car toyotaCar = new Car("Toyota", 240);
+        toyotaCar.startEngine(3);
+        System.out.println("[" + toyotaCar.getName() + "] Current speed is " + toyotaCar.getSpeed());
+        toyotaCar.speedUp(2);
+        System.out.println("[" + toyotaCar.getName() + "] Current speed is " + toyotaCar.getSpeed());
+        toyotaCar.speedDown(3);
+        System.out.println("[" + toyotaCar.getName() + "] Current speed is " + toyotaCar.getSpeed());
+        toyotaCar.stopEngine();
+        System.out.println("[" + toyotaCar.getName() + "] Current speed is " + toyotaCar.getSpeed());
+
+        Car bmwCar = new Car("BMW", 240);
+        bmwCar.startEngine(2);
+        System.out.println("[" + bmwCar.getName() + "] Current speed is " + toyotaCar.getSpeed());
+        bmwCar.speedUp(2);
+        System.out.println("[" + bmwCar.getName() + "] Current speed is " + toyotaCar.getSpeed());
+        bmwCar.speedDown(3);
+        System.out.println("[" + bmwCar.getName() + "] Current speed is " + toyotaCar.getSpeed());
+        bmwCar.stopEngine();
+        System.out.println("[" + bmwCar.getName() + "] Current speed is " + toyotaCar.getSpeed());
     }
 }
