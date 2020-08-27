8120a5c4c81472a984d5a9f38338c725c0a46d8c
Sergiu Chilat <chilatsergiu@gmail.com>
2020-08-27 20:13:19 +0300

    Overloading

---
+++ src/Lesson12/Main.java
@@ -0,0 +1,19 @@
+package Lesson12;
+
+public class Main {
+    public static void main(String[] args) {
+        OverloadingTest ot = new OverloadingTest();
+        System.out.println(ot.add(1, 2));
+        System.out.println(ot.add('x', 'y'));
+        System.out.println(ot.add('x', "String"));
+        System.out.println(ot.add(1.222F, "String"));
+
+        System.out.println(ot.drawFigure());
+        System.out.println(ot.drawFigure(new Point(2, 3)));
+        System.out.println(ot.drawFigure(new Point(2), new Point(5, 8)));
+        System.out.println(ot.drawFigure(new Point(2, 3), new Point(), new Point(0, 8)));
+
+
+
+    }
+}
---
+++ src/Lesson12/OverloadingTest.java
@@ -0,0 +1,51 @@
+package Lesson12;
+
+class Point {
+    public int x;
+    public int y;
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(int x) {
+        this();
+        this.x = x;
+    }
+
+    public Point(int x, int y) {
+        this(x);
+        this.y = y;
+    }
+}
+
+public class OverloadingTest {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public String add(char a, char b) {
+        return a + "" + b;
+    }
+
+    public <T> String add(T a, T b) {
+        return "Unknown type [" + a.getClass() + " ]for 'a'" + ",\n Unknown type [" + b.getClass() + "] for 'b'";
+    }
+
+    public String drawFigure() {
+        return "Unknown figure";
+    }
+
+    public String drawFigure(Point point) {
+        return "Point:[" + point.x + ", " + point.y + "]";
+    }
+
+    public String drawFigure(Point pointStart, Point pointEnd) {
+        return "Segment: [" + pointStart.x + ", " + pointStart.y + "], [" + pointEnd.x + ", " + pointEnd.y + "]";
+    }
+
+    public String drawFigure(Point pointA, Point pointB, Point pointC) {
+        return "Triangle: [" + pointA.x + ", " + pointA.y + "], [" + pointB.x + ", " + pointB.y + "], [" + pointC.x + ", " + pointC.y + "]";
+    }
+}
