5da2ac9f1954937ec9442d6f4080b1f6e7e84b95
Sergiu Chilat <chilatsergiu@gmail.com>
2020-09-08 11:42:08 +0300

    DAO sample (+ console dialog)

--- src/dao/Main.java
+++ src/dao/Main.java
@@ -3,38 +3,72 @@ package dao;
 import dao.model.Student;
 import dao.service.StudentService;
 
+import java.util.Scanner;
+
 public class Main {
+    StudentService service = new StudentService();
     public static void main(String[] args) {
-        StudentService service = new StudentService();
-        if(service.create(new Student("Ion Creanga", 123)) != null) {
-            System.out.println("Create success");
-        } else {
-            System.out.println("Create error");
+        Main app = new Main();
+        int option;
+        do{
+            option = app.selectOption();
+            switch (option) {
+                case 1 -> app.create();
+                case 2 -> app.find();
+                case 3 -> app.list();
+                case 4 -> app.update();
+                case 5 -> app.delete();
+                default -> System.out.println("Invalid option selected");
+            }
+        }while (option != 0);
+    }
+
+    private int selectOption() {
+        System.out.println("Select option:");
+        System.out.println("1. Add student:");
+        System.out.println("2. Find student:");
+        System.out.println("3. List of students:");
+        System.out.println("4. Update student:");
+        System.out.println("5. Delete student:");
+        System.out.println("0. Exit:");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextInt();
+        } catch (Exception e) {
+            return 0;
         }
+    }
 
+    private void create(){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Student name:");
+            String name = scanner.nextLine();
+            System.out.println("Student age:");
+            Integer age = scanner.nextInt();
+            if(service.create(new Student(name, age)) != null) {
+                System.out.println("Create success");
+            } else {
+                System.out.println("Create error");
+            }
+        } catch (Exception e){
+            System.out.println("Error: Invalid input data");
+        }
+    }
 
-        int id = 2;
+    private void find(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Student ID to find:");
+        Integer id = scanner.nextInt();
         Student student = service.read(id);
         if(student.getId() != null){
             System.out.println("Student with id [" + id + "]: " + student);
         } else {
             System.out.println("Student with id [" + id + "] not found");
         }
+    }
 
-
-        if(service.create(new Student("Mihai Eminescu", 120)) != null) {
-            System.out.println("Create success");
-        } else {
-            System.out.println("Create error");
-        }
-
-        student = service.read(id);
-        if(student.getId() != null){
-            System.out.println("Student with id [" + id + "]: " + student);
-        } else {
-            System.out.println("Student with id [" + id + "] not found");
-        }
-
+    private void list(){
         System.out.println("Students list: ");
         Student[] students = service.read();
         for (Student studentItem: students){
@@ -42,33 +76,37 @@ public class Main {
                 System.out.println(studentItem.toString());
             }
         }
+    }
 
-        if(service.update(2, new Student("Vasile Alexandri", 100)) != null) {
-            System.out.println("Student with id [" + id + "] updated");
-        } else {
-            System.out.println("Student with id [" + id + "] not found");
-        }
-
-        System.out.println("Students list: ");
-        students = service.read();
-        for (Student studentItem: students){
-            if(studentItem != null) {
-                System.out.println(studentItem.toString());
+    private void update(){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Student ID to update:");
+            Integer id = scanner.nextInt();
+            //todo nu functioneaza corect daca nu se creeaza o instanta noua la scaner
+            scanner = new Scanner(System.in);
+            System.out.println("Student name:");
+            String name = scanner.nextLine();
+            System.out.println("Student age:");
+            Integer age = scanner.nextInt();
+            if(service.update(id, new Student(name, age)) != null) {
+                System.out.println("Student with id [" + id + "] updated");
+            } else {
+                System.out.println("Student with id [" + id + "] not found");
             }
+        } catch (Exception e){
+            System.out.println("Error: Invalid input data");
         }
+    }
 
-        if(service.delete(2)) {
+    public void delete(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Student ID to delete:");
+        Integer id = scanner.nextInt();
+        if(service.delete(id)) {
             System.out.println("Student with id [" + id + "] deleted");
         } else {
             System.out.println("Student with id [" + id + "] not found");
-        }
-
-        System.out.println("Students list: ");
-        students = service.read();
-        for (Student studentItem: students){
-            if(studentItem != null) {
-                System.out.println(studentItem.toString());
-            }
         }
     }
 }
